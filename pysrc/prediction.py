64b2487708e3684dfeb6c6887670e1e4e4572027
ozakiryota <kb22056@gmail.com>
2020-06-05 01:41:51 +0900

    updated

--- pysrc/prediction.py
+++ pysrc/prediction.py
@@ -4,6 +4,8 @@ import rospy
 from sensor_msgs.msg import Image
 
 import torch
+from torchvision import models
+import torch.nn as nn
 
 def callback(msg):
     # print("test")
@@ -13,6 +15,20 @@ def prediction():
     rospy.init_node('prediction', anonymous=True)
     rospy.Subscriber("/image_raw", Image, callback)
     rospy.spin()
+
+    ## network
+    use_pretrained = True
+    net = models.vgg16(pretrained=use_pretrained)
+    net.features[26] = nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(0, 0))
+    net.features = nn.Sequential(*list(net.features.children())[:-3])
+    net.classifier = nn.Sequential(
+        nn.Linear(in_features=73728, out_features=18, bias=True),
+        nn.ReLU(True),
+        nn.Linear(in_features=18, out_features=3, bias=True)
+    )
+    print(net)
+
+net.eval()
 
 if __name__ == '__main__':
     prediction()
