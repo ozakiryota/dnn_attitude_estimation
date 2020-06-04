190c5e79896cbb2c521f556ae81069a52033d344
ozakiryota <kb22056@gmail.com>
2020-06-04 23:06:12 +0900

    updated

---
+++ docker/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+image_name="dnn_attitude_estimation"
+
+xhost +
+nvidia-docker run -it --rm \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--net=host \
+	$image_name:latest
