5ef110210ae1d0ca426dd4e3ba0e22e73fcf3aaf
ozakiryota <kb22056@gmail.com>
2020-08-04 19:39:42 +0900

    updated

--- nvidia_docker1/run.sh
+++ nvidia_docker1/run.sh
@@ -9,6 +9,7 @@ nvidia-docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--net=host \
+	-v $root_path/../weights:/home/ros_catkin_ws/src/$image_name/weights \
 	-v $root_path/../pysrc:/home/ros_catkin_ws/src/$image_name/pysrc \
-	$image_name:nvidia_docker1 \
-	/bin/bash /home/regression_prediction.sh
+	$image_name:nvidia_docker1
+	# /bin/bash /home/regression_prediction.sh
