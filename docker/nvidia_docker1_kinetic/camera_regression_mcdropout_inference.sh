de98c306e6299b16aa6782cf414dcc404fc9755f
ozakiryota <kb22056@gmail.com>
2021-02-16 04:25:24 +0900

    updated

---
+++ docker/nvidia_docker1_kinetic/camera_regression_mcdropout_inference.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+image_name="dnn_attitude_estimation"
+root_path=$(pwd)
+
+xhost +
+nvidia-docker run -it --rm \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--net=host \
+	-v $root_path/../../weights:/home/ros_catkin_ws/src/$image_name/weights \
+	-v $root_path/../../pysrc:/home/ros_catkin_ws/src/$image_name/pysrc \
+	--env="OMP_NUM_THREADS=1" \
+	$image_name:nvidia_docker1_kinetic \
+	bash -c "\
+		source /opt/ros/kinetic/setup.bash; \
+		source /home/ros_catkin_ws/devel/setup.bash; \
+		source /home/catkin_build_ws/install/setup.bash --extend; \
+		roslaunch dnn_attitude_estimation camera_regression_mcdropout_inference.launch"
--- pysrc/camera/camera_regression_mcdropout_inference.py
+++ pysrc/camera/camera_regression_mcdropout_inference.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import Image as ImageMsg
 from geometry_msgs.msg import Vector3Stamped
+from sensor_msgs.msg import Imu
 
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -25,9 +26,9 @@ class AttitudeEstimation:
         self.frame_id = rospy.get_param("/frame_id", "/base_link")
         print("self.frame_id = ", self.frame_id)
         ## parameter-dnn
-        weights_path = rospy.get_param("/weights_path", "../../weights/regression.pth")
+        weights_path = rospy.get_param("/weights_path", "../../weights/weights.pth")
         print("weights_path = ", weights_path)
-        resize = rospy.get_param("/resize", 112)
+        resize = rospy.get_param("/resize", 224)
         print("resize = ", resize)
         mean_element = rospy.get_param("/mean_element", 0.5)
         print("mean_element = ", mean_element)
@@ -35,18 +36,15 @@ class AttitudeEstimation:
         print("std_element = ", std_element)
         ## subscriber
         self.sub_color_img = rospy.Subscriber("/color_image", ImageMsg, self.callbackColorImage, queue_size=1, buff_size=2**24)
-        self.sub_depth_img = rospy.Subscriber("/depth_image", ImageMsg, self.callbackDepthImage, queue_size=1, buff_size=2**24)
         ## publisher
         self.pub_vector = rospy.Publisher("/dnn/g_vector", Vector3Stamped, queue_size=1)
+        self.pub_accel = rospy.Publisher("/dnn/g_vector_with_cov", Imu, queue_size=1)
         ## msg
         self.v_msg = Vector3Stamped()
+        self.accel_msg = Imu()
         ## cv
         self.bridge = CvBridge()
         self.color_img_cv = np.empty(0)
-        self.depth_img_cv = np.empty(0)
-        ## flag
-        self.got_new_color_img = False
-        self.got_new_depth_img = False
         ## dnn
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print("self.device = ", self.device)
@@ -84,24 +82,9 @@ class AttitudeEstimation:
             self.color_img_cv = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             print("msg.encoding = ", msg.encoding)
             print("self.color_img_cv.shape = ", self.color_img_cv.shape)
-            self.got_new_color_img = True
-            if self.got_new_color_img and self.got_new_depth_img:
-                outputs = self.dnnPrediction()
-                self.inputToMsg(outputs)
-                self.publication(msg.header.stamp)
-        except CvBridgeError as e:
-            print(e)
-
-    def callbackDepthImage(self, msg):
-        try:
-            self.depth_img_cv = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
-            print("msg.encoding = ", msg.encoding)
-            print("self.depth_img_cv.shape = ", self.depth_img_cv.shape)
-            self.got_new_depth_img = True
-            if self.got_new_color_img and self.got_new_depth_img:
-                outputs = self.dnnPrediction()
-                self.inputToMsg(outputs)
-                self.publication(msg.header.stamp)
+            outputs = self.dnnPrediction()
+            self.inputToMsg(outputs)
+            self.publication(msg.header.stamp)
         except CvBridgeError as e:
             print(e)
 
@@ -109,16 +92,16 @@ class AttitudeEstimation:
         print("----------")
         start_clock = rospy.get_time()
         ## inference
-        inputs_color, inputs_depth = self.transformImage()
+        inputs_color = self.transformImage()
         print("inputs_color.size() = ", inputs_color.size())
-        print("inputs_depth.size() = ", inputs_depth.size())
-        outputs = self.net(inputs_color, inputs_depth)
-        print("outputs = ", outputs)
+        list_outputs = []
+        for _ in range(self.num_mcsampling):
+            outputs = self.net(inputs_color)
+            list_outputs.append(outputs.cpu().detach().numpy())
+        print("list_outputs = ", list_outputs)
         ## reset
-        self.got_new_color_img = False
-        self.got_new_depth_img = False
         print("Period [s]: ", rospy.get_time() - start_clock)
-        return outputs
+        return list_outputs
 
     def transformImage(self):
         ## color
@@ -126,12 +109,7 @@ class AttitudeEstimation:
         color_img_tensor = self.img_transform(color_img_pil)
         inputs_color = color_img_tensor.unsqueeze_(0)
         inputs_color = inputs_color.to(self.device)
-        ## depth
-        self.depth_img_cv = self.depth_img_cv.astype(np.float32)
-        depth_img_tensor = torch.from_numpy(self.depth_img_cv)
-        inputs_depth = depth_img_tensor.unsqueeze_(0).unsqueeze_(0)
-        inputs_depth = inputs_depth.to(self.device)
-        return inputs_color, inputs_depth
+        return inputs_color
 
     def cvToPIL(self, img_cv):
         img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
@@ -139,12 +117,44 @@ class AttitudeEstimation:
         return img_pil
 
     def inputToMsg(self, outputs):
-        ## tensor to numpy
-        outputs = outputs[0].cpu().detach().numpy()
+        ## get mean and covariance matrix
+        mean = np.array(outputs).mean(0)
+        print("mean.shape = ", mean.shape)
+        cov = self.getCovMatrix(outputs)
         ## Vector3Stamped
-        self.v_msg.vector.x = -outputs[0]
-        self.v_msg.vector.y = -outputs[1]
-        self.v_msg.vector.z = -outputs[2]
+        self.v_msg.vector.x = -mean[0]
+        self.v_msg.vector.y = -mean[1]
+        self.v_msg.vector.z = -mean[2]
+        ## Imu
+        self.inputNanToImuMsg(self.accel_msg)
+        self.accel_msg.linear_acceleration.x = -mean[0]
+        self.accel_msg.linear_acceleration.y = -mean[1]
+        self.accel_msg.linear_acceleration.z = -mean[2]
+        for i in range(cov.size):
+            self.accel_msg.linear_acceleration_covariance[i] = cov[i//3, i%3]
+        ## print
+        print("mean = ", mean)
+        print("cov = ", cov)
+
+    def inputNanToImuMsg(self, imu):
+        imu.orientation.x = math.nan
+        imu.orientation.y = math.nan
+        imu.orientation.z = math.nan
+        imu.orientation.w = math.nan
+        imu.angular_velocity.x = math.nan
+        imu.angular_velocity.y = math.nan
+        imu.angular_velocity.z = math.nan
+        imu.linear_acceleration.x = math.nan
+        imu.linear_acceleration.y = math.nan
+        imu.linear_acceleration.z = math.nan
+        for i in range(len(imu.linear_acceleration_covariance)):
+            imu.orientation_covariance[i] = math.nan
+            imu.angular_velocity_covariance[i] = math.nan
+            imu.linear_acceleration_covariance[i] = math.nan
+
+    def getCovMatrix(self, outputs):
+        cov = np.cov(outputs[:, 0, :], rowvar=False, bias=True)
+        return cov
 
     def publication(self, stamp):
         print("delay[s]: ", (rospy.Time.now() - stamp).to_sec())
@@ -152,6 +162,10 @@ class AttitudeEstimation:
         self.v_msg.header.stamp = stamp
         self.v_msg.header.frame_id = self.frame_id
         self.pub_vector.publish(self.v_msg)
+        ## Imu
+        self.accel_msg.header.stamp = stamp
+        self.accel_msg.header.frame_id = self.frame_id
+        self.pub_accel.publish(self.accel_msg)
 
 def main():
     ## Node
